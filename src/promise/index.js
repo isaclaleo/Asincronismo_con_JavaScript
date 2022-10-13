f000deff97ffd165036c7bb8b9f7f86f4973ece8
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-13 14:31:34 -0500

    clase12

--- src/promise/index.js
+++ src/promise/index.js
@@ -21,3 +21,13 @@ countCows.then((result) => {
 
 // Un callback devuelve una función en los parámetros, cuando llamamos varias veces un callback, estaremos colocando muchas lineas de código y sería engorroso, por eso nacen las promesas, éstas optimizan y permiten leer mejor el código con pocas lineas.
 // Las promesas son asíncronas, por lo que el código continuará su ejecución normalmente y luego dirá si la promesa se resolvió o se rechazó. Por lo que varias promesas pueden llegar a entrar en ejecución al mismo tiempo.
+
+// Reto 
+
+export function reto(time, messege) {
+    return new Promise((resolve) => {
+        window.setTimeout(() => {
+            resolve(messege);
+        }, time)
+    })
+}
