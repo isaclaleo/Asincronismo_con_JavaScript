214361a672f876c1ab3c5578f494a09cbbc12372
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-13 17:31:35 -0500

    clase14 cambios de post

--- src/promise/challenge-post.js
+++ src/promise/challenge-post.js
@@ -15,11 +15,11 @@ function postData(ursApi, data){
     return response;
 }
 const data ={
-    "title": "1302",
-    "price": 1302,
-    "description": "A description",
+    "title": "",
+    "price": 1234,
+    "description": "Kratos is a father again. As mentor and protector to Atreus, a son determined to earn his respect, he is forced to deal with and control the rage that has long defined him while out in a very dangerous world with his son",
     "categoryId": 1,
-    "images": ["https://placeimg.com/640/480/any"]
+    "images": ["https://images.pexels.com/photos/459762/pexels-photo-459762.jpeg?cs=srgb&dl=pexels-pixabay-459762.jpg&fm=jpg"]
   }
 
   postData(`${API}/products`, data)
